f3e6139d3b3e74c790e54614c7cf5bdf46b94c44
dathdka <dathdka@gmail.com>
2022-12-03 11:08:14 +0700

    redeploy

--- shared/timBaoDong.js
+++ shared/timBaoDong.js
@@ -1,4 +1,4 @@
-export function timBaoDong (baoDongTN = [], F = []) {
+export function timBaoDong (baoDongTN = [], F = [{L: '', R: ''}]) {
     for (let thuocTinh of F) {
       if (
         thuocTinh.L.split('').filter((el) => !baoDongTN.includes(el)).length === 0 &&
